8d45638330a0442a9e0ece372fc0f55e5af3ccc3
Rosert2019 <tresordjonga@mail.ru>
2021-10-01 15:09:39 +0100

    Routiers modelviewset

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
-from shop.views import CategoryAPIView, ProductAPIView
+from rest_framework import routers
+ 
+from shop.views import CategoryViewset, ProductViewset
+
+from rest_framework import routers
+
+
+# Ici nous créons notre routeur
+
+router = routers.SimpleRouter()
+# Puis lui déclarons une url basée sur le mot clé ‘category’ et notre view
+# afin que l’url générée soit celle que nous souhaitons ‘/api/category/’
+router.register('category', CategoryViewset, basename='category')
+router.register('product', ProductViewset, basename='product')
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/category/', CategoryAPIView.as_view()),
-    path('api/product/', ProductAPIView.as_view()),
+    path('api/', include(router.urls)),
 ]
